a600917172e90abd1137132d7f1651862c7a5baa
Musa Bagja <musabagja@Musas-MacBook-Air.local>
2022-07-21 15:00:58 +0700

    done fix

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -40,7 +40,7 @@ export default function DetailPage() {
 
   useEffect(() => {
     if (profile)
-      setTransact((prev: any) => ({ ...prev, participant: profile.fullname, phoneNumber: profile.phoneNumber.slice(3), email: profile.email }))
+      setTransact((prev: any) => ({ ...prev, participant: profile.fullname, phoneNumber: profile.phoneNumber ? profile.phoneNumber.slice(3) : '', email: profile.email }))
   }, [profile])
 
   return (
